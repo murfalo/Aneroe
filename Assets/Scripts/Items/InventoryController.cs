5427ed33d1cd45347ff14830d9f6199c982ce245
Murfalo <murphycangelo@u.northwestern.edu>
2017-02-02 13:45:33 -0600

    Implement limited, click-based UI

--- Assets/Scripts/Items/InventoryController.cs
+++ Assets/Scripts/Items/InventoryController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿
 using System;
 using UnityEngine;
@@ -9,6 +10,17 @@ using InventoryEvents;
 /// <summary>
 /// This class manages the inventory UI as well as a simple event system
 /// to allow easy inventory updating.
+=======
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// This class manages 
+>>>>>>> Implement limited, click-based UI
 /// </summary>
 public class InventoryController : MonoBehaviour, IPointerClickHandler
 {
@@ -17,6 +29,7 @@ public class InventoryController : MonoBehaviour, IPointerClickHandler
     [SerializeField] GameObject UISlot;
 
     /// <section>Item currently selected by the player.</section>
+<<<<<<< HEAD
     private GameObject selected { get; set; }
 
     /// <section>Event for an item moving in the inventory.</section>
@@ -24,6 +37,9 @@ public class InventoryController : MonoBehaviour, IPointerClickHandler
 
     /// <section>Original parent of the currently selected item.</section>
     private Transform parent { get; set; }
+=======
+    public GameObject selected { get; set; }
+>>>>>>> Implement limited, click-based UI
 
     /// <section>Initializes the inventory to the size of the currently active character.</section>
     public void Start()
@@ -34,7 +50,11 @@ public class InventoryController : MonoBehaviour, IPointerClickHandler
             if (i % 2 == 0)
                 Destroy(newSlot.transform.GetChild(0).gameObject);
             newSlot.name = "Slot." + i.ToString();
+<<<<<<< HEAD
             newSlot.transform.SetParent(transform.GetChild(0).transform);
+=======
+            newSlot.transform.SetParent(transform);
+>>>>>>> Implement limited, click-based UI
         }
     }
 
@@ -49,10 +69,15 @@ public class InventoryController : MonoBehaviour, IPointerClickHandler
     /// <param name="target">Either a UI item or UI slot to select an item from.</param>
     private void SelectItem(GameObject target)
     {
+<<<<<<< HEAD
         if (target.CompareTag("UISlot")) selected = null;
         if (!target.CompareTag("UIItem")) return;
         selected = target;
         parent = selected.transform.parent;
+=======
+        if (!target.CompareTag("UIItem") && !target.CompareTag("UISlot")) return;
+        selected = target;
+>>>>>>> Implement limited, click-based UI
         selected.transform.SetParent(selected.GetComponentInParent<Canvas>().transform);
         target.GetComponent<Image>().raycastTarget = false;
     }
@@ -63,6 +88,7 @@ public class InventoryController : MonoBehaviour, IPointerClickHandler
     {
         var prevSelected = selected;
         selected.GetComponent<Image>().raycastTarget = true;
+<<<<<<< HEAD
         if (target.CompareTag("UIItem") || target.CompareTag("UISlot"))
         {
             var newParent = (target.CompareTag("UIItem")) ? target.transform.parent : target.transform;
@@ -77,6 +103,18 @@ public class InventoryController : MonoBehaviour, IPointerClickHandler
             selected = null;
         }
 
+=======
+        if (target.CompareTag("UIItem"))
+        {
+            prevSelected.transform.SetParent(target.transform.parent);
+            SelectItem(target);
+        }
+        else if (target.CompareTag("UISlot"))
+        {
+            selected = null;
+            prevSelected.transform.SetParent(target.transform);
+        }
+>>>>>>> Implement limited, click-based UI
         prevSelected.transform.position = prevSelected.transform.parent.transform.position;
     }
 
@@ -91,6 +129,7 @@ public class InventoryController : MonoBehaviour, IPointerClickHandler
         else
             SelectItem(target);
     }
+<<<<<<< HEAD
 
     /// <section>Publishes the itemMoved event if an item has changed positions in the inventory.</section>
     private void OnItemMoved(GameObject go, Transform prevParent, Transform newParent = null)
@@ -104,3 +143,6 @@ public class InventoryController : MonoBehaviour, IPointerClickHandler
             itemMoved(this, new InventoryEvents.ItemMovedEventArgs(item, prevSlot, newSlot));
     }
 }
+=======
+}
+>>>>>>> Implement limited, click-based UI
