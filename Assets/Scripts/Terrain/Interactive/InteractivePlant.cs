965f6639f72dd2812d90118aee5d2d33101a7582
ChristopherKober <christopherkober2018@u.northwestern.edu>
2017-03-04 22:55:14 -0600

    Added breakable plants

--- Assets/Scripts/Terrain/Interactive/InteractivePlant.cs
+++ Assets/Scripts/Terrain/Interactive/InteractivePlant.cs
@@ -8,18 +8,17 @@ public class InteractivePlant : Tile
 	public Sprite fullTileSprite, brokenTileSprite;
 	SpriteRenderer sRend;
 	bool broken;
-	public bool isPast;
+	Collider2D coll;
 	public string[] usableItemPrefabNames;
 	public GameObject drop;
 
 	public override bool CanUseItem(Item item) {
 		// If you're not wielding something, interaction is always allowed
-		print ("Trying to Using Item");
-		if (item == null) return true;
-		var itemType = item.GetType();
-		var canUse = itemType == System.Type.GetType("Item") ? usableItemPrefabNames.Any(n => n == item.prefabName) : usableItemTypes.Any(i => i == itemType);
-		interactState = canUse ? Entity.CharacterState.Interacting : default(Entity.CharacterState);
-		return canUse;
+		if (typeof(Weapon) == item.GetType ()) {
+			Item i;
+			this.UseItem (item,out i);
+		}
+		return false;
 	}
 
 	// Use this for initialization
@@ -28,6 +27,7 @@ public class InteractivePlant : Tile
 		usableItemPrefabNames = new string[0];
 		usableItemTypes = new System.Type[0];
 		sRend = GetComponent<SpriteRenderer> ();
+		coll = GetComponent<Collider2D> ();
 		broken = false;
 	}
 
@@ -38,10 +38,14 @@ public class InteractivePlant : Tile
 
 	public override void UseItem (Item item, out Item newItem)
 	{
-		print ("Using Item");
 		newItem = null;
-		sRend.sprite = brokenTileSprite;
+		if (typeof(Weapon) != item.GetType ()) {
+			return;
+		}
+
+		sRend.enabled = false;
 		broken = true;
+		coll.enabled = false;
 		GameObject dropItem = (GameObject)Instantiate (drop);
 		Item i = dropItem.GetComponent<Item> ();
 		i.Setup();
@@ -51,12 +55,14 @@ public class InteractivePlant : Tile
 	public override Hashtable Save() {
 		Hashtable tsd = new Hashtable (); 
 		tsd.Add ("broken", broken);
+		tsd.Add ("col_enabled", coll.enabled);
 		return tsd;
 	}
 
 	public override void Load(Hashtable tsd) {
 		broken = (bool)tsd ["broken"];
-		sRend.sprite = broken ? brokenTileSprite : fullTileSprite;
+		coll.enabled = (bool)tsd ["col_enabled"];
+		sRend.enabled = broken ? false : true;
 	}
 }
 
