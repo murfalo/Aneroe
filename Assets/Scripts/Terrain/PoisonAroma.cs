ad95fb9b08f3b7f8c759b5b4c99119772b87df7d
angelomatni <angelomatni2018@u.northwestern.edu>
2017-03-08 18:23:17 -0600

    Made the boss harder

--- Assets/Scripts/Terrain/PoisonAroma.cs
+++ Assets/Scripts/Terrain/PoisonAroma.cs
@@ -3,10 +3,27 @@ using System.Collections;
 
 public class PoisonAroma : MonoBehaviour
 {
+	float timer = 0;
+	const float BUFFER = 1;
+	bool damaged;
+
+	void Update() {
+		if (damaged) {
+			timer += Time.deltaTime;
+			if (timer > BUFFER) {
+				timer = 0;
+				damaged = false;
+			}
+		}
+	}
+
 	void OnTriggerStay2D(Collider2D col) {
+		if (damaged)
+			return;
 		BossEntity boss = col.GetComponentInParent<BossEntity> ();
 		if (boss != null) {
-			boss.Damage (10, 1, .3f, 0f);
+			boss.Damage (10, 1, .1f, 0f);
+			damaged = true;
 		}
 	}
 }
